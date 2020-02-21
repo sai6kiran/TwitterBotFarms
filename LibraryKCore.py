7f22453d0bdd3c9b5afec44f09bc70defec25450
root <root@ctn-dev-03.asset.ibm.cloud>
2020-02-21 18:19:34 +0000

    Latest change to LibraryKCore.py

--- LibraryKCore.py
+++ LibraryKCore.py
@@ -752,6 +752,7 @@ df_lst = pd.DataFrame(columns=["tweetid","userid","is_retweet","retweet_userid",
 dfo = pd.DataFrame(columns=["tweetid","userid","is_retweet","retweet_userid","retweet_tweetid"])
 pd.set_option('display.max_columns', 100)
 
+'''
 #Add Retweet Bots to csv file:
 for df_ in df:
 	#pdb.set_trace()
@@ -797,6 +798,7 @@ def converter(x):
     else:
         return x
 '''
+'''
 #Add originator bots to csv file:
 dfa = pd.read_csv("/root/.encrypted/.pythonSai/my_parsed.csv", sep=",", header=None, usecols=[0,1,2,3,4], chunksize=2000, skiprows=1, names=["tweetid","userid","is_retweet","retweet_userid","retweet_tweetid"])
 for dfa in df:
@@ -828,15 +830,30 @@ for dfn in dfa:
 dfo.to_csv('my_parsed.csv', mode='a', header=False)
 '''
 
-'''
-G=nx.DiGraph()
-uid = []
-for index, row in df_lst.iterrows():
-	if(row['retweet_userid'] not in uid and row['retweet_userid'] is not None):
-		G.add_node(row['retweet_userid'])
-	if(row['userid'] not in uid):
-		G.add_node(row['userid'])
+#Constructing the graph:
+dfn = pd.read_csv("/root/.encrypted/.pythonSai/my_parsed.csv", sep=",", header=None, chunksize=2000, names=["tweetid","userid","is_retweet","retweet_userid","retweet_tweetid"])
 
+G=nx.DiGraph()
+tmp = 0
+cnt = 0
+for df_ in dfn:
+	#t0 = time.time()
+	tmp +=1
+	print(len(df_))
+	#for index, row in df_.iterrows():
+		#cnt+=1
+		#if(row["retweet_userid"] == ''):
+			#print(row, tmp, cnt)
+			#break
+	df_lst = df_.loc[df_["retweet_userid"].map(lambda x: print(x, tmp) if x!='' else "false")]
+	#t1 = time.time()
+	print("************************************************************")
+	#print(t1-t0)
+	#if(row['retweet_userid'] not in uid and row['retweet_userid'] is not None):
+		#G.add_node(row['retweet_userid'])
+	#if(row['userid'] not in uid):
+		#G.add_node(row['userid'])
+'''
 for index, row in df_lst.iterrows():
 	G.add_edges_from([(row['userid'],row['retweet_userid'])])
 
@@ -874,6 +891,7 @@ else:
 #print(df_final)
 # In[ ]:
 '''
+
 
 
 
